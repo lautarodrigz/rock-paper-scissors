b1c318227fb9abef60c19be25619776aaa6df483
lautaro <lautirod12@gmail.com>
2024-04-13 11:46:43 -0300

    Change return of playRound() and create playGame()

--- javascript/script.js
+++ javascript/script.js
@@ -11,38 +11,89 @@ function playRound(playerSelection, computerSelection) {
         case "rock":
             switch (computerSelection) {
                 case "Rock":
-                    return "Tie! Rock vs Rock"
+                    console.log("Tie! Rock vs Rock")
+                    return 3
                 case "Paper":
-                    return "You lose! Paper beats Rock"
+                    console.log("You lose! Paper beats Rock")
+                    return 2
                 case "Scissors":
-                    return "You win! Rock beats Scissors"
+                    console.log("You win! Rock beats Scissors")
+                    return 1
                 default:
-                    return "Error"
+                    return console.error("Error");
             }
         case "paper":
             switch (computerSelection) {
                 case "Rock":
-                    return "You win! Paper beats Rock"
-                case "Paper":
-                    return "Tie! Paper vs Paper"
+                    console.log("You win! Paper beats Rock")
+                    return 1
+              case "Paper":
+                    console.log("Tie! Paper vs Paper")
+                    return 3
                 case "Scissors":
-                    return "You lose! Scissors beat Paper"
+                    console.log("You lose! Scissors beat Paper")
+                    return 2
                 default:
-                    return "Error"
+                    return console.error("Error");
             }
         case "scissors":
             switch (computerSelection) {
                 case "Rock":
-                    return "You lose! Rock beats Scissors"
+                    console.log("You lose! Rock beats Scissors")
+                    return 2
                 case "Paper":
-                    return "You win! Scissors beat Paper"
+                    console.log("You win! Scissors beat Paper")
+                    return 1
                 case "Scissors":
-                    return "Tie! Scissors vs Scissors"
+                    console.log("Tie! Scissors vs Scissors")
+                    return 3
                 default:
-                    return "Error"
+                    return console.error("Error");
             }
         default:
-            console.log("Error")
-            break;
+            return console.error("Error");
     }
 }
+
+function playGame() {
+    let playerChoice = "";
+    let wins = 0;
+    let loses = 0;
+    let ties = 0;
+    for (let i = 0; i < 5; i++) {
+        do {
+            playerChoice = prompt("Choose: Rock, Paper or Scissors").toLowerCase();
+            if (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors") {
+                alert("That is not a valid option! Please choose again.");
+            }
+        } while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
+        switch (playRound(playerChoice, getComputerChoice())) {
+            case 1:
+                wins++;
+                break;
+            case 2:
+                loses++;
+                break;
+            case 3:
+                ties++;
+                break;    
+            default:
+                return console.error("Error");
+        }
+        
+    }
+    console.log(`
+    ${wins} wins
+    ${loses} loses
+    ${ties} ties
+    `)
+    if (wins > loses) {
+        console.log(`You won the game! ${wins} wins vs ${loses} loses`)
+    } else if (loses > wins) {
+        console.log(`You lost the game! ${wins} wins vs ${loses} loses`)
+    } else {
+        console.log(`You tied the game! ${wins} wins vs ${loses} loses`)
+    }
+}
+
+playGame()
