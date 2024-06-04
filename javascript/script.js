a251be54abb159c43c24c4c67f4e3a601950a821
lautaro <lautirod12@gmail.com>
2024-06-04 18:49:14 -0300

    Comment playGame function

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,6 @@ function getComputerChoice() {
     let result = choices[(Math.floor(Math.random() * 3))]
     return result
 }
-
 function playRound(playerSelection, computerSelection) {
     console.log("You selected: " + playerSelection.toLowerCase())
     console.log("The computer selected: " + computerSelection.toLowerCase())
@@ -54,46 +53,44 @@ function playRound(playerSelection, computerSelection) {
             return console.error("Error");
     }
 }
+// function playGame() {
+//     let playerChoice = "";
+//     let wins = 0;
+//     let loses = 0;
+//     let ties = 0;
+//     for (let i = 0; i < 5; i++) {
+//         do {
+//             playerChoice = prompt("Choose: Rock, Paper or Scissors").toLowerCase();
+//             if (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors") {
+//                 alert("That is not a valid option! Please choose again.");
+//             }
+//         } while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
+//         switch (playRound(playerChoice, getComputerChoice())) {
+//             case 1:
+//                 wins++;
+//                 break;
+//             case 2:
+//                 loses++;
+//                 break;
+//             case 3:
+//                 ties++;
+//                 break;    
+//             default:
+//                 return console.error("Error");
+//         }
+//     }
+//     console.log(`
+//     ${wins} wins
+//     ${loses} loses
+//     ${ties} ties
+//     `)
+//     if (wins > loses) {
+//         console.log(`You won the game! ${wins} wins vs ${loses} loses`)
+//     } else if (loses > wins) {
+//         console.log(`You lost the game! ${wins} wins vs ${loses} loses`)
+//     } else {
+//         console.log(`You tied the game! ${wins} wins vs ${loses} loses`)
+//     }
+// }
 
-function playGame() {
-    let playerChoice = "";
-    let wins = 0;
-    let loses = 0;
-    let ties = 0;
-    for (let i = 0; i < 5; i++) {
-        do {
-            playerChoice = prompt("Choose: Rock, Paper or Scissors").toLowerCase();
-            if (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors") {
-                alert("That is not a valid option! Please choose again.");
-            }
-        } while (playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
-        switch (playRound(playerChoice, getComputerChoice())) {
-            case 1:
-                wins++;
-                break;
-            case 2:
-                loses++;
-                break;
-            case 3:
-                ties++;
-                break;    
-            default:
-                return console.error("Error");
-        }
-        
-    }
-    console.log(`
-    ${wins} wins
-    ${loses} loses
-    ${ties} ties
-    `)
-    if (wins > loses) {
-        console.log(`You won the game! ${wins} wins vs ${loses} loses`)
-    } else if (loses > wins) {
-        console.log(`You lost the game! ${wins} wins vs ${loses} loses`)
-    } else {
-        console.log(`You tied the game! ${wins} wins vs ${loses} loses`)
-    }
-}
-
-playGame()
+// playGame()
